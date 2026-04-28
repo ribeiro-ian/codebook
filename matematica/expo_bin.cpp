ca51ca02e12bc90dc1c7f12eb515576a9eabef86
ian <ian.ribeiro4@gmail.com>
2026-04-28 10:34:40 -0300

    add Exponenciacao Binaria

---
+++ matematica/expo_bin.cpp
@@ -0,0 +1,12 @@
+// a^b O(logN)
+ll exp(ll a, ll b, const ll M = 1e9+7) {
+    a %= M;
+    ll res = 1;
+    
+    while(b) {
+        if (b % 2) res = res * a % M;
+        a = a * a % M;
+        b /= 2;
+    }
+    return res;
+}
